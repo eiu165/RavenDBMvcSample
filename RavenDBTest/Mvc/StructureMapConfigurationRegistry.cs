166a772b0f00da891dfb191443bd0b34990fc5fc
Michael Friis <friism@gmail.com>
2012-05-24 16:22:20 -0700

    add structuremap stuff

---
+++ RavenDBTest/Mvc/StructureMapConfigurationRegistry.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using Raven.Abstractions.Data;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using StructureMap.Configuration.DSL;
+
+namespace RavenDBTest.Mvc
+{
+	public sealed class StructureMapConfigurationRegistry : Registry
+	{
+		public StructureMapConfigurationRegistry()
+		{
+			var environmentName = ConfigurationManager.AppSettings.Get("Environment");
+
+			ForSingletonOf<IDocumentStore>().Use(() => GetDocumentStore(environmentName));
+
+			For<IDocumentSession>().HybridHttpOrThreadLocalScoped().Use(context =>
+			{
+				return context.GetInstance<IDocumentStore>().OpenSession();
+			});
+		}
+
+		private IDocumentStore GetDocumentStore(string environmentName)
+		{
+			IDocumentStore store;
+
+			switch (environmentName)
+			{
+				case "Debug":
+					store = GetEmbeddableDocumentStore();
+					break;
+				case "Release":
+					store = GetConnectionstringDocumentStore();
+					break;
+				default:
+					throw new ArgumentException("environmentName");
+			}
+
+			store.Initialize();
+			return store;
+		}
+
+		private IDocumentStore GetEmbeddableDocumentStore()
+		{
+			var store = new EmbeddableDocumentStore();
+			store.DataDirectory = "App_Date";
+			return store;
+		}
+
+		private IDocumentStore GetConnectionstringDocumentStore()
+		{
+			var parser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionStringName("RavenDB");
+			parser.Parse();
+
+			var store = new DocumentStore
+			{
+				ApiKey = parser.ConnectionStringOptions.ApiKey,
+				Url = parser.ConnectionStringOptions.Url,
+			};
+
+			return store;
+		}
+	}
+}
