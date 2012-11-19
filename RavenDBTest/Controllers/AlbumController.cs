bc416cf0d8589a8d65e30c8afead011835aead1f
eiu165 <eiu165@gmail.com>
2012-11-19 14:41:27 -0600

    added album

---
+++ RavenDBTest/Controllers/AlbumController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RavenDBTest.Model;
+
+namespace RavenDBTest.Controllers
+{
+    public class AlbumController :    BaseController
+    {
+   
+        public ActionResult Index()
+        {
+            return View(RavenSession.Query<Album>());
+        }
+    }
+}
--- RavenDBTest/Model/Thingy.cs
+++ RavenDBTest/Model/Thingy.cs
@@ -1,8 +1,14 @@
 ﻿
 namespace RavenDBTest.Model
 {
-	public class Thingy
-	{
-		public string Name { get; set; }
-	}
+    public class Thingy
+    {
+        public string Name { get; set; }
+    }
+    public class Album
+    {
+        public string Title { get; set; }
+        public string AlbumArtUrl { get; set; }
+        public float Price { get; set; }
+    }
 }
